b5e102265cb0036c9f55f522e7c6d0c5a67bfe08
curryppah <98072080+curryppah@users.noreply.github.com>
2022-07-02 13:12:45 +0900

    remove unused elements

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -1,14 +1,7 @@
 import './AboutUs.scss'
 import config from '../config.json'
-import { FaPlus } from 'react-icons/fa';
 
 const Content = () => {
-  const clickHandler = () => {
-    if (config.form_url) {
-      window.location.href = config.form_url
-    }
-  }
-
 
   return (
     <div className="about-us">
