4b016321f7f3d6897489465dd9b1939597bee589
curryppah <98072080+curryppah@users.noreply.github.com>
2022-07-03 16:44:56 +0900

    modify description

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -8,10 +8,10 @@ const Content = () => {
           <div className="image"><img src={`${process.env.PUBLIC_URL}/logo_teapot.svg`} alt=""/></div>
           <div className="logo">Concafemap for stamp rally</div>
         </div>
+        
+        <h2>アプリについて</h2>
         <p><b>Concafemap for stamp rally</b> はとあるスタンプラリー企画をより楽しめるようにお手伝いをする地図アプリです（非公式）</p>
-
-        <h2>アプリの特徴と使い方</h2>
-        <ul>
+        <ul style="margin-left: 2rem">
           <li>スタンプラリー参加店舗の場所がわかる</li>
           <li>店舗までの道順と距離がわかる</li>
         </ul>
@@ -20,7 +20,7 @@ const Content = () => {
         <p>スタンプラリー参加店舗の一覧です。スマホの位置情報をONにしておけば近い順に表示されます。</p>
         
         <h3>エリア</h3>
-        <p>大須・名駅・栄・その他のエリアにしぼって表示します</p>
+        <p>大須・名駅・栄・その他のエリアにしぼって一覧表示します</p>
 
         <h3>写真から探す</h3>
         <p>店舗のロゴおよび写真をタップすると店舗詳細が表示されます</p>
@@ -32,9 +32,8 @@ const Content = () => {
         <h2>お問い合わせ</h2>
         <p>情報の訂正やご意見などがありましたら<address>potatician@gmail.com</address>までお知らせください。<br />
           現状では位置ずれが多いため随時調整します。</p>
-        <p>基本的に店舗のロゴおよび写真は権利の関係で掲載しておりません。
-          もし提供してくださる店舗がありましたらロゴや写真などを1枚お送りください。
-          横幅350px以上でお願いします。</p>
+        <p>基本的に店舗のロゴおよび写真は掲載しておりません。
+          もし提供してくださるお店がありましたらロゴや写真などをお送りください（横幅350px以上）。</p>
 
         <h2>Powered by Geolonia</h2>
         <p>本アプリの開発には株式会社Geolonia様提供の Geolonia PWAマップ を利用しています。</p>
